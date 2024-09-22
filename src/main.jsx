4757a8ba2aea553e03c28b6efeb1225e562c7b01
Rafsan12 <rafsan.web.2022@gmail.com>
2024-09-22 10:00:05 +0600

    make career-hub project, add react-router-dom,add daisyUl and tailwindcss

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Root from "./components/Root/Root";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
