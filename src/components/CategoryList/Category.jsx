e1177c2c197270362d87ea3e684ace3f911429d8
Rafsan12 <rafsan.web.2022@gmail.com>
2024-09-23 07:25:26 +0600

    category  update

--- src/components/CategoryList/Category.jsx
+++ src/components/CategoryList/Category.jsx
@@ -4,7 +4,7 @@ const Category = ({ category }) => {
   return (
     <div className="card bg-base-100  shadow-xl mt-14 mb-8">
       <figure>
-        <img src={logo} alt="Shoes" />
+        <img src={logo} alt={category_name} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{category_name}</h2>
